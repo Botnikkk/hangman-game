012a318d3b16c01fd0d523ca843f7f907f9092d2
Nikhil Chaudhary <Nikhil.ace.chd@gmail.com>
2022-08-27 01:25:46 +0530

    leaderboard

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import operator
 import pickle as p
 
 file_path = "user_data"
@@ -5,7 +6,6 @@ file_path = "user_data"
 
 def centre(symbol, title) :
     print(str(symbol)*(64-int((len(title)/2))) + title + (64-int((len(title)/2)))*str(symbol) + 2*"\n")
-
 
 def ans_check(option_list) :
 
@@ -17,7 +17,6 @@ def ans_check(option_list) :
         print("Not a valid answer !")
         answer = input("Choose a option\n- ")
     return answer
-
 
 def signup():
 
@@ -36,7 +35,7 @@ def signup():
         except EOFError :
             break
     
-    centre(symbol="=", title="Sign up page")
+    centre(symbol="=", title=" Sign up page ")
 
     #taking id input
     id_input = input("Enter ID that you wish to register wish\n- ")
@@ -58,10 +57,9 @@ def signup():
     centre(symbol="=", title="")
     login()
 
-
 def login() :
 
-    centre(symbol="=", title="Login page")
+    centre(symbol="=", title=" Login page ")
 
     file = open(file_path, "br+")
 
@@ -114,14 +112,27 @@ def login() :
     else : 
         centre(symbol="=", title="You were logged out")
 
-
 def homescreen(user):
-    option_list = ["play", "leaderboard", "guide and info", "hint shop", "user info"]
+
+    centre(symbol="=", title=" Home Page ")
+    option_list = ["play", "leaderboard", "guide and info", "hint shop", "user info", "log out"]
     answer = ans_check(option_list)
-    if answer == option_list[2] :
+
+    if answer == option_list[0] :
+        a=1
+
+    elif answer == option_list[1] :
+        leaderboard(user)
+
+    elif answer == option_list[2] :
         info(user)
-    
-    
+
+    elif answer == option_list[4] :
+        user_info(user)
+
+    elif answer == option_list[5] :
+        centre(symbol="=", title=" You were logged out ")
+   
 def info(user):
     file = open("info.txt", "r+")
     file.seek(0,0)
@@ -134,14 +145,88 @@ def info(user):
 
     if answer == "back" :
         homescreen(user)
+
+def user_info(user) :
+    file = open(file_path, "rb+")
+    centre(symbol="=", title=" User Info")
+    option_list = ["my info", "another user info", "back"]
+    answer = ans_check(option_list)
+
+    if answer == option_list[0] : 
+        while True : 
+
+            try : 
+                values = p.load(file) 
+                if values["id"] == user : 
+                    string = "Info of {}".format(user)
+                    centre(symbol= " ",title=string)
+                    print("Hints = {hints}\nPoints = {points}\nScore = {score}".format(hints=values["hints"], points=values["points"], score = values["score"]))
+
+            except EOFError :
+                break
+
+    elif answer == option_list[1] : 
+        if user in ["nikhil", "ekta"] :
+
+            input_id = input("Enter the ID you wish to search for\n- ")
+            found = 0
+            while True : 
+
+                try : 
+                    values = p.load(file) 
+                    if values["id"] == input_id.lower() :
+                        string = "Info of {}".format(input_id)
+                        centre(symbol= " ",title=string)
+                        print("Hints = {hints}\nPoints = {points}\nScore = {score}".format(hints=values["hints"], points=values["points"], score = values["score"]))
+                        found = 1
+                except EOFError :
+                    break
+            if found == 0 :
+                print("No user with such ID was found")
+        else  : 
+            print("Only admins can use this option !")
+    elif answer == option_list[2] :
+        homescreen(user)
     
+    answer = ans_check(option_list=["back"])
+    homescreen(user)
+    file.close()
 
+def leaderboard(user) :
+    centre("=", " Leaderboard ")
+    file = open(file_path, "rb+")
 
+    user_list = {}
+    while True : 
 
+            try : 
+                value = p.load(file) 
+                user_list.update({value["id"] : value["points"]})
+                if value["id"] == user : 
+                    user_data = value
+            except EOFError :
+                break
+    sorted_dic = sorted(user_list.items(), key=operator.itemgetter(1), reverse=True)
+    user_rank = 0
+    rank = 1
+    for i in sorted_dic :
+        if rank < 11 :
+            string = "Rank {rank} -> {id}".format(rank=rank,id=i[0]) + " "*(10- len(i[0])) + "points : {points}".format(points=i[1])
+            print(32*" ", string)
+            if i[0] == user : 
+                user_rank = rank
+            rank += 1
+        else :
+            break
+    if user_rank == 0 : 
+        user_rank = " * "
+    print(2*"\n")
+    centre(" ", "Your rank")
+    string = "Rank {rank} -> {id}".format(rank=user_rank,id=user_data["id"]) + " "*(10- len(i[0]))+ "points : {points}".format(points=user_data["points"])
+    print(32*" ", string)
 
-
-
-
+    answer = ans_check(option_list=["back"])
+    homescreen(user)
 
 
 

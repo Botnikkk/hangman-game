14e48005f3d3905acfdf91498a8913d398837b56
Nikhil Chaudhary <Nikhil.ace.chd@gmail.com>
2022-08-27 11:34:42 +0530

    updates with UI

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ file_path = "user_data"
 
 
 def centre(symbol, title) :
+    #aligns the title in centre with symbols around it
     print(str(symbol)*(64-int((len(title)/2))) + title + (64-int((len(title)/2)))*str(symbol) + 2*"\n")
 
 def ans_check(option_list) :
 
+    #prints and detetcs the answers and returns the choose answer
     for i in option_list : 
         centre(symbol=" ", title=("* " + str(i)))
 
@@ -63,7 +65,7 @@ def login() :
 
     file = open(file_path, "br+")
 
-    #storing existing ids
+    #sorting existing ids
     id_data = []
     pass_data = []
     while True : 
@@ -83,6 +85,18 @@ def login() :
     input_id = input("Enter your ID\n- ")
     if input_id in id_data and id_trials > -1 : 
             index = id_data.index(input_id)
+            #checking pass
+            input_pass = input("Enter your password\n- ")
+            if input_pass.lower() != pass_data[index] :
+
+                while input_pass != pass_data[index] and pass_trials > 0 : 
+
+                    print("Incorrect password!\nTrial left = {trials}".format(trials=pass_trials))
+                    input_pass = input("Enter your password\n- ")
+                    pass_trials -= 1
+
+                    if input_pass == pass_data[index] and id_trials > -1 :
+                        break
     else:
         while input_id not in id_data and id_trials > 0 :
             print("ID wasn't found\nTrials left = {trials}".format(trials=id_trials))
@@ -92,32 +106,36 @@ def login() :
             if input_id in id_data and id_trials > -1 : 
                 index = id_data.index(input_id)
                 break
+        #checking pass
+        input_pass = input("Enter your password\n- ")
+        if input_pass.lower() != pass_data[index] :
+
+            while input_pass != pass_data[index] and pass_trials > 0 : 
+
+                print("Incorrect password!\nTrial left = {trials}".format(trials=pass_trials))
+                input_pass = input("Enter your password\n- ")
+                pass_trials -= 1
+
+                if input_pass == pass_data[index] and id_trials > -1 :
+                    break
     
-    #checking pass
-    input_pass = input("Enter your password\n- ")
-    if input_pass.lower() != pass_data[index] :
-
-        while input_pass != pass_data[index] and pass_trials > 0 : 
-
-            print("Incorrect password!\nTrial left = {trials}".format(trials=pass_trials))
-            input_pass = input("Enter your password\n- ")
-            pass_trials -= 1
-
-            if input_pass == pass_data[index] and id_trials > -1 :
-                break
     
+    #welcome screen
     if input_id in id_data and input_pass == pass_data[index] :
-        centre(symbol="=", title=" Welcome {user} ".format(user=input_id))
+        centre(symbol="-", title=" Welcome {user} ".format(user=input_id))
         homescreen(input_id)
     else : 
-        centre(symbol="=", title="You were logged out")
+        centre(symbol="=", title=" You were logged out ")
 
 def homescreen(user):
-
+    #printing home bar
     centre(symbol="=", title=" Home Page ")
+
+    #printing and detecting option choices
     option_list = ["play", "leaderboard", "guide and info", "hint shop", "user info", "log out"]
     answer = ans_check(option_list)
 
+    #performing tasks based on choice
     if answer == option_list[0] :
         a=1
 
@@ -134,12 +152,15 @@ def homescreen(user):
         centre(symbol="=", title=" You were logged out ")
    
 def info(user):
+
+    #fetching and printing the file
     file = open("info.txt", "r+")
     file.seek(0,0)
     lines = file.readlines()
     for i in lines : 
         print(i,end="")
     
+    #returning to homescreen
     option_list = ["back"]
     answer = ans_check(option_list)
 
@@ -147,27 +168,34 @@ def info(user):
         homescreen(user)
 
 def user_info(user) :
+
+    #opening file and printing and detecting options
     file = open(file_path, "rb+")
-    centre(symbol="=", title=" User Info")
+    centre(symbol="=", title=" User Info ")
     option_list = ["my info", "another user info", "back"]
     answer = ans_check(option_list)
 
     if answer == option_list[0] : 
+
+        #iterating through file to find and print data
         while True : 
 
             try : 
                 values = p.load(file) 
                 if values["id"] == user : 
                     string = "Info of {}".format(user)
-                    centre(symbol= " ",title=string)
+                    centre(symbol= "-",title=string)
                     print("Hints = {hints}\nPoints = {points}\nScore = {score}".format(hints=values["hints"], points=values["points"], score = values["score"]))
 
             except EOFError :
                 break
 
     elif answer == option_list[1] : 
-        if user in ["nikhil", "ekta"] :
 
+        #admin check
+        if user in ["nikhil", "ekta"] :
+            
+            #admin
             input_id = input("Enter the ID you wish to search for\n- ")
             found = 0
             while True : 
@@ -176,7 +204,7 @@ def user_info(user) :
                     values = p.load(file) 
                     if values["id"] == input_id.lower() :
                         string = "Info of {}".format(input_id)
-                        centre(symbol= " ",title=string)
+                        centre(symbol= "-",title=string)
                         print("Hints = {hints}\nPoints = {points}\nScore = {score}".format(hints=values["hints"], points=values["points"], score = values["score"]))
                         found = 1
                 except EOFError :
@@ -184,6 +212,8 @@ def user_info(user) :
             if found == 0 :
                 print("No user with such ID was found")
         else  : 
+
+            #not admin
             print("Only admins can use this option !")
     elif answer == option_list[2] :
         homescreen(user)
@@ -193,9 +223,12 @@ def user_info(user) :
     file.close()
 
 def leaderboard(user) :
+
+    #pinting heading 
     centre("=", " Leaderboard ")
     file = open(file_path, "rb+")
 
+    #collecting user data with points
     user_list = {}
     while True : 
 
@@ -206,24 +239,28 @@ def leaderboard(user) :
                     user_data = value
             except EOFError :
                 break
+
+    #sorting and printing the user data
     sorted_dic = sorted(user_list.items(), key=operator.itemgetter(1), reverse=True)
     user_rank = 0
     rank = 1
     for i in sorted_dic :
         if rank < 11 :
             string = "Rank {rank} -> {id}".format(rank=rank,id=i[0]) + " "*(10- len(i[0])) + "points : {points}".format(points=i[1])
-            print(32*" ", string)
+            print(50*" ", string)
             if i[0] == user : 
                 user_rank = rank
             rank += 1
         else :
             break
+
+    #printing self rank
     if user_rank == 0 : 
         user_rank = " * "
     print(2*"\n")
     centre(" ", "Your rank")
     string = "Rank {rank} -> {id}".format(rank=user_rank,id=user_data["id"]) + " "*(10- len(i[0]))+ "points : {points}".format(points=user_data["points"])
-    print(32*" ", string)
+    print(50*" ", string, "\n")
 
     answer = ans_check(option_list=["back"])
     homescreen(user)
@@ -238,11 +275,20 @@ except :
     file = open(file_path, "bw+")
 file.close()
 
+string = """
+================================================================================================================================
+|                                                                                                                              |
+|                        ░█─░█ ─█▀▀█ ░█▄─░█ ░█▀▀█ ░█▀▄▀█ ─█▀▀█ ░█▄─░█ 　 ░█▀▀█ ─█▀▀█ ░█▀▄▀█ ░█▀▀▀                              | 
+|                        ░█▀▀█ ░█▄▄█ ░█░█░█ ░█─▄▄ ░█░█░█ ░█▄▄█ ░█░█░█ 　 ░█─▄▄ ░█▄▄█ ░█░█░█ ░█▀▀▀                              |
+|                        ░█─░█ ░█─░█ ░█──▀█ ░█▄▄█ ░█──░█ ░█─░█ ░█──▀█ 　 ░█▄▄█ ░█─░█ ░█──░█ ░█▄▄▄                              |
+|                                                                                                                              |
+================================================================================================================================
+"""
+
 while 1 < 2 :
-    answer  = input("Are you a existing user?\n- ")
-    while answer.lower() not in ["yes", "no"] :
-        print("Invalid answer")
-        answer  = input("Are you a existing user?\n- ")
+    print(string)
+    centre( " ","Are you a existing user ?")
+    answer  = ans_check(option_list=["yes", "no"])   
     if answer == "yes" :
         login()
     else :
